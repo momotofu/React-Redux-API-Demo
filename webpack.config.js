e555dbb54aa4bea55032ec88eebd39fb16c71fb9
Christopher Reece <mrkita.reece@gmail.com>
2018-10-01 16:05:14 +0900

    updated stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,64 @@
 var path = require('path')
 var webpack = require('webpack')
 var env = process.env.NODE_ENV
+const stylus = require('stylus')
 
-module.exports = {
-  entry: path.join(__dirname, './src/index.js'),
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/dist/',
-    filename: 'frontend.js'
-  },
-  devServer: {
-    port: 9000,
-    inline: true
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
-      {
-        test: /\.styl$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        loader: 'style-loader!css-loader!stylus-loader'
-      },
-      { test: /\.svg/, loader: 'svg-url-loader'}
+module.exports = (env, options) => {
+  return {
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath: '/dist/',
+      filename: 'frontend.js'
+    },
+    resolve: {
+      extensions: ['.js', '.styl']
+    },
+    devServer: {
+      port: 9000,
+      inline: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react'],
+              plugins: [require('babel-plugin-transform-object-rest-spread')]
+            },
+          }
+        },
+        {
+          test: /\.styl$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          use: [
+            'style-loader',
+            'css-loader',
+            'stylus-loader?paths=node_modules/bootstrap-stylus/stylus/'
+          ]
+        },
+        { test: /\.svg/,
+          use: [
+            { loader: 'svg-url-loader' }
+          ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            { loader: 'file-loader' }
+          ]
+        }
+      ]
+    },
+    plugins: [
+     new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(env)
+      })
     ]
-  },
-  plugins: [
-   new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    })
-  ]
+  }
 }
