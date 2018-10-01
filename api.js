6e474ae38d54918978880b99f6a75e82b8986193
Christopher Reece <mrkita.reece@gmail.com>
2018-10-01 16:40:13 +0900

    added dist path for fonts to express api

--- api.js
+++ api.js
@@ -9,6 +9,10 @@ module.exports = router => {
     res.sendFile(path.resolve(__dirname, `dist/${req.params.filename}`))
   })
 
+  router.get('/dist/:filename', async (req, res) => {
+    res.sendFile(path.resolve(__dirname, `dist/${req.params.filename}`))
+  })
+
   router.get('/', async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.sendFile(path.resolve(__dirname, 'index.html'))
