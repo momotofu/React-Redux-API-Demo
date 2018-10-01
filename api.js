0567c3b118f43a3bed9d56b9b1da664f3d58eef2
Christopher Reece <mrkita.reece@gmail.com>
2018-10-01 09:54:18 +0900

    logging filepath

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const path = require('path')
  */
 module.exports = router => {
   router.get('/assets/js/:filename', async (req, res) => {
+    console.log('path: ', path.resolve(__dirname, `dist/${req.params.filename}`))
     res.sendFile(path.resolve(__dirname, `dist/${req.params.filename}`))
   })
 
